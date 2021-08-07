937215d1fddf8d21ca196c1f75d86426ac933569
mihail <mihail.soltan@protonmail.ch>
2021-08-07 15:59:29 +0200

    cuisine fix

--- api/cuisine/controllers/cuisine.js
+++ api/cuisine/controllers/cuisine.js
@@ -1,8 +1,20 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.cuisine.search(ctx.query);
+    } else {
+      entities = await strapi.services.cuisine.find(ctx.query, ['posts', 'post.id']);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.cuisine }));
+  },
+};
