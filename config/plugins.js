7c9ec9b0a0bfc188243363b549f365fa7c449261
mihail <mihail.soltan@protonmail.ch>
2021-08-14 15:57:24 +0200

    added images to strapi

--- config/plugins.js
+++ config/plugins.js
@@ -2,9 +2,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID_S3,
         secretAccessKey: process.env.AWS_ACCESS_SECRET,
-        region: process.env.AWS_REGION,
+        region: process.env.AWS_REGION_S3,
         params: {
           Bucket: process.env.AWS_BUCKET_NAME,
           StorageClass: env('AWS_S3_STORAGE_CLASSES')
